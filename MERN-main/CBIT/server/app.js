35539708bcad916e826f8446000164accfe44762
Snikitha <snikithagovindu@gmail.com>
2024-11-07 10:40:33 +0530

    first commit

---
+++ MERN-main/CBIT/server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors=require('cors')
+const studentRouter = require('./routes/students')
+
+//below is the cloud db
+const url = 'mongodb+srv://guntisamhitha16:8985080910@merncrud.pxurr.mongodb.net/?retryWrites=true&w=majority&appName=MERNCRUD';
+//const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url = 'mongodb://127.0.0.1:27017/CBIT'
+const app = express()
+mongoose.connect(url)
+const con = mongoose.connection
+
+
+con.on('open', () =>
+{
+console.log('connected...')
+})
+app.use(cors())
+app.use(express.json())
+
+app.use('/students',studentRouter)
+app.listen(9000, () =>
+{
+console.log('Server started')
+})
